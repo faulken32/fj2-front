7ee646e30fbe41e5673b02372b769901351aaa57
nicoals <canicatti.nicolas@gmail.com>
2023-04-18 14:09:26 +0200

    candidate wip

--- src/main/webapp/app/entities/candidate/candidate.model.ts
+++ src/main/webapp/app/entities/candidate/candidate.model.ts
@@ -1,15 +1,19 @@
 import dayjs from 'dayjs/esm';
 import { ICandidatePreference } from 'app/entities/candidate-preference/candidate-preference.model';
 import { ICv } from 'app/entities/cv/cv.model';
+import { IUser } from '../user/user.model';
+import { IAddress } from '../address/address.model';
 
 export interface ICandidate {
   id: number;
+  user?: IUser | null;
   userId?: number | null;
+  address?: IAddress | null;
   name?: string | null;
   phone?: string | null;
   valid?: boolean | null;
   validUntil?: dayjs.Dayjs | null;
-  candidatePreference?: Pick<ICandidatePreference, 'id'> | null;
+  candidatePreference?: Pick<ICandidatePreference, 'id' | 'distance'> | null;
   cv?: Pick<ICv, 'id' | 'name'> | null;
 }
 
