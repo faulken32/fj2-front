6cd8c4743c823c79db48833ee8e655d06b5bd113
nicoals <canicatti.nicolas@gmail.com>
2023-05-19 11:12:24 +0200

    candidate wip

--- src/main/webapp/app/entities/candidate-preference/candidate-preference.model.ts
+++ src/main/webapp/app/entities/candidate-preference/candidate-preference.model.ts
@@ -1,6 +1,9 @@
+import {IJobSubCat} from "../job-sub-cat/job-sub-cat.model";
+
 export interface ICandidatePreference {
   id: number;
   distance?: number | null;
+  jobSubCat?: IJobSubCat | null;
 }
 
 export type NewCandidatePreference = Omit<ICandidatePreference, 'id'> & { id: null };
--- src/main/webapp/app/entities/user/user.model.ts
+++ src/main/webapp/app/entities/user/user.model.ts
@@ -1,6 +1,8 @@
 export interface IUser {
   id: number;
   login?: string;
+  imageUrl?:string;
+  email?:string;
 }
 
 export class User implements IUser {
